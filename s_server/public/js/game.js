f58f69279ce12eab7e6dd9a3da74f9528f8fcab4
Michael Chen <mikaloramik@gmail.com>
2013-08-27 01:06:43 -0400

    make window not scroll after end

--- s_server/public/js/game.js
+++ s_server/public/js/game.js
@@ -23,4 +23,21 @@ $(document).ready(function() {
       socket.emit('sendChat', gameID, message);
     }
   });
+
+
+  $('#event-log').bind('mousewheel DOMMouseScroll', function(e) {
+    var scrollTo = null;
+
+    if (e.type == 'mousewheel') {
+      scrollTo = (e.originalEvent.wheelDelta * -1);
+    }
+    else if (e.type == 'DOMMouseScroll') {
+      scrollTo = 40 * e.originalEvent.detail;
+    }
+
+    if (scrollTo) {
+      e.preventDefault();
+      $(this).scrollTop(scrollTo + $(this).scrollTop());
+    }
+  });
 });
